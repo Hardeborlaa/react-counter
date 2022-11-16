c8c0c60c2cc04ceb7bab39a987ae2c5b125d5ae9
Adeleye Opeyemi <adebola786@gmail.com>
2022-11-16 23:00:05 +0100

    added style

--- src/index.js
+++ src/index.js
@@ -17,15 +17,15 @@ function App() {
   };
 
   return (
-    <main>
-      <section>
-        <h1>Counter</h1>
-        <div>{count}</div>
-        <div>
-          <button onClick={Increase}>Increase</button>
-          <button onClick={Decrease}>Decrease</button>
+    <main className = "grid justify-center bg-purple-800 text-white h-screen">
+      <section className='m-auto text-center'>
+        <h1 className='text-7xl'>Counter</h1>
+        <div className='text-5xl py-12'>{count}</div>
+        <div className='flex justify-center gap-5'>
+          <button className='bg-yellow-400 hover:bg-yellow-600 py-3 px-10 font-bold text-xl rounded-2xl' onClick={Increase}>Increase</button>
+          <button className='bg-pink-400 hover:bg-pink-600 py-3 px-10 font-bold text-xl rounded-2xl'  onClick={Decrease}>Decrease</button>
         </div>
-        <button onClick={Reset}>Reset</button>
+        <button className='bg-blue-400 hover:bg-blue-600 py-3 px-20 font-bold text-xl my-10 rounded-2xl' onClick={Reset}>Reset</button>
       </section>
     </main>
   )
